436822c9331aa6b03c8df1c1c280e89a04cdc967
Lucca Bassoli <lucca.bassoli2020@gmail.com>
2021-09-18 05:10:38 -0300

    update controller version

--- src/controllers/VersionController.ts
+++ src/controllers/VersionController.ts
@@ -2,7 +2,7 @@ import { Response, Request } from 'express'
 
 class VersionController {
     show(request: Request, response: Response) {
-        return response.send({
+        return response.status(200).json({
             'API Version': '1.0.0-ALPHA',
             'Seu IP': request.ip,
         })
