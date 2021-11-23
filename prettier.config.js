cfea054b48622713319bf57d9fddbc4b919177f7
Lucca Bassoli <65320125+BassoliCodes@users.noreply.github.com>
2021-11-23 11:38:18 -0300

    Delete prettier.config.js

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    singleQuote: true,
-    trailingComma: 'all',
-    arrowParens: 'avoid',
-    tabWidth: 4,
-    semi: false,
-    bracketSpacing: true,
-    printWidth: 100,
-};
